90c3f2ee7c8877bac4adfda6dba976123b8af6d6
Sam Whited <sam@samwhited.com>
2015-12-14 20:01:39 -0600

    Improve server docs with purpose

--- server/doc.go
+++ server/doc.go
@@ -1,3 +1,5 @@
 // The server package is used to manage C2S, S2S, and component connections to
-// your XMPP server or component. It does most of the heavy lifting.
+// your XMPP server or component. It provides a higher level API for creating
+// and managing XMPP servers (without digging down in to the nitty gritty XML
+// details).
 package server
