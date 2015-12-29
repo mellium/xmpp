4c30512f2050002a70a2c5485ce74579288dd9d8
Sam Whited <sam@samwhited.com>
2015-12-29 00:31:30 -0600

    Add basic xmpp package docs
    Add copyright header to all go files

--- doc.go
+++ doc.go
@@ -1,1 +1,7 @@
+// Copyright 2014 Sam Whited.
+// Use of this source code is governed by the BSD 2-clause license that can be
+// found in the LICENSE file.
+
+// The xmpp package provides functionality from the Extensible Messaging and
+// Presence Protocol (more commonly known as XMPP).
 package xmpp // import "bitbucket.org/mellium/xmpp"
--- server/doc.go
+++ server/doc.go
@@ -1,5 +1,9 @@
+// Copyright 2014 Sam Whited.
+// Use of this source code is governed by the BSD 2-clause license that can be
+// found in the LICENSE file.
+
 // The server package is used to manage C2S, S2S, and component connections to
 // your XMPP server or component. It provides a higher level API for creating
 // and managing XMPP servers (without digging down in to the nitty gritty XML
 // details).
-package server
+package server // import "bitbucket.org/mellium/xmpp/server"
