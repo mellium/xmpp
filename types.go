81e19341d1b1a626031b135b58b769c156ee2bbf
Sam Whited <sam@samwhited.com>
2016-01-02 23:07:23 -0600

    Don't stringify types

--- types.go
+++ types.go
@@ -1,10 +1,8 @@
-// Copyright 2014 Sam Whited.
+// Copyright 2015 Sam Whited.
 // Use of this source code is governed by the BSD 2-clause license that can be
 // found in the LICENSE file.
 
 package xmpp
-
-//go:generate stringer -type=messageType,presenceType,iqType
 
 type messageType int
 
