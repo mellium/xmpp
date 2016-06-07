5c84d20d4cd79722935ed0bcc8b9c1ba0f3e7bdd
Sam Whited <sam@samwhited.com>
2016-06-06 19:24:32 -0500

    Use context package compatible with Go 1.6

--- lookup.go
+++ lookup.go
@@ -5,7 +5,6 @@
 package xmpp
 
 import (
-	"context"
 	"encoding/json"
 	"encoding/xml"
 	"net"
@@ -15,6 +14,7 @@ import (
 	"strings"
 	"sync"
 
+	"golang.org/x/net/context"
 	"golang.org/x/net/context/ctxhttp"
 	"mellium.im/xmpp/internal"
 	"mellium.im/xmpp/jid"
