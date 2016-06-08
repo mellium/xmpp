48af4be36ddcdd952d55e5d2bc6a53b5af237ba6
Sam Whited <sam@samwhited.com>
2016-06-08 14:31:11 -0500

    Update comment on one example with notes

--- examples/echobot_client.go
+++ examples/echobot_client.go
@@ -18,6 +18,14 @@ func main() {
 	// New is variadic and takes an arbitrary number of client.Option's
 	c, err := client.New(laddr, client.TLSConfig(tls.Config{}))
 
+	// TODO: Most libraries probably let you match on xpath or something so you
+	//       can, eg. only handle messages that contain a <thread/> child element.
+	//       I don't want that overhead (and there's no built in xpath anyways),
+	//       so maybe we need a way to get/trigger the default handler if the
+	//       method doesn't meet some sort of validation here within our handler?
+	//       Maybe handlers should be a middleware style pattern and we can fall
+	//       through to the next handler in the chain, or return and short
+	//       circuit?
 	c.HandleIQ(xml.Name{"urn:xmpp:ping", "ping"}, func(event client.Event) {
 		log.Println("Got ping, sending pong.")
 		event.Client.Reply(event.Stanza, xmpp.IQ{Type: ResultIQ})
