2e8ee785e93947daf29845a994d7f76657c837f3
Sam Whited <sam@samwhited.com>
2016-06-07 22:45:27 -0500

    Add a test for DialClient

---
+++ dial_test.go
@@ -0,0 +1,20 @@
+// Copyright 2016 Sam Whited.
+// Use of this source code is governed by the BSD 2-clause license that can be
+// found in the LICENSE file.
+
+package xmpp
+
+import (
+	"testing"
+
+	"mellium.im/xmpp/jid"
+)
+
+func TestDialClientPanicsIfNilContext(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Error("Expected Dial to panic when passed a nil context.")
+		}
+	}()
+	DialClient(nil, "tcp", jid.MustParse("feste@shakespeare.lit"))
+}
