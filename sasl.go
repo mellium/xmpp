6ba624028a885011a5bc367834a2a53a99fede35
Sam Whited <sam@samwhited.com>
2016-07-09 15:02:36 -0500

    Add basic structure for SASL auth stream feature

---
+++ sasl.go
@@ -0,0 +1,59 @@
+// Copyright 2016 Sam Whited.
+// Use of this source code is governed by the BSD 2-clause license that can be
+// found in the LICENSE file.
+
+package xmpp
+
+import (
+	"context"
+	"encoding/xml"
+	"fmt"
+	"io"
+
+	"mellium.im/sasl"
+)
+
+// Sasl returns a stream feature for performing authentication using the Simple
+// Authentication and Security Layer (SASL) as defined in RFC 4422. It panics if
+// no mechanisms are specified.
+func Sasl(mechanisms ...*sasl.Mechanism) *StreamFeature {
+	if len(mechanisms) == 0 {
+		panic("xmpp: Must specify at least 1 SASL mechanism")
+	}
+	return &StreamFeature{
+		Name:       xml.Name{Space: "urn:ietf:params:xml:ns:xmpp-sasl", Local: "mechanisms"},
+		Necessary:  Secure,
+		Prohibited: Authn,
+		List: func(ctx context.Context, conn io.Writer) (req bool, err error) {
+			req = true
+			_, err = fmt.Fprint(conn, `<mechanisms xmlns='urn:ietf:params:xml:ns:xmpp-sasl'>`)
+			if err != nil {
+				return
+			}
+			for _, m := range mechanisms {
+				if _, err = fmt.Fprint(conn, `<mechanism>`); err != nil {
+					return
+				}
+				if err = xml.EscapeText(conn, []byte(m.Name)); err != nil {
+					return
+				}
+				if _, err = fmt.Fprint(conn, `</mechanism>`); err != nil {
+					return
+				}
+			}
+			_, err = fmt.Fprint(conn, `</mechanisms>`)
+			return
+		},
+		Parse: func(ctx context.Context, d *xml.Decoder, start *xml.StartElement) (bool, interface{}, error) {
+			panic("Parsing mechanisms not yet implemented")
+		},
+		Negotiate: func(ctx context.Context, conn *Conn, data interface{}) (SessionState, error) {
+			if (conn.state & Received) == Received {
+				panic("sendMechanisms not yet implemented")
+			} else {
+				panic("readMechanisms not yet implemented")
+			}
+			return Authn | StreamRestartRequired, nil
+		},
+	}
+}
