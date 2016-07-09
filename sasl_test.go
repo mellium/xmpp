4abc82142b25b589760fbe4ffd6f7c70b4548327
Sam Whited <sam@samwhited.com>
2016-07-09 15:42:01 -0500

    Add test for listing SASL mechanisms

--- sasl_test.go
+++ sasl_test.go
@@ -5,7 +5,12 @@
 package xmpp
 
 import (
+	"bytes"
+	"context"
+	"encoding/xml"
 	"testing"
+
+	"mellium.im/sasl"
 )
 
 func TestSASLPanicsNoMechanisms(t *testing.T) {
@@ -15,4 +20,53 @@ func TestSASLPanicsNoMechanisms(t *testing.T) {
 		}
 	}()
 	_ = SASL()
+}
+
+func TestSASLList(t *testing.T) {
+	var b bytes.Buffer
+	s := SASL(sasl.Plain("", "user", "pass"), sasl.ScramSha256("user", "pass"))
+	req, err := s.List(context.Background(), &b)
+	switch {
+	case err != nil:
+		t.Fatal(err)
+	case req != true:
+		t.Error("Expected SASL to be a required feature")
+	}
+
+	// Mechanisms should be printed exactly thus:
+	if !bytes.Contains((&b).Bytes(), []byte(`<mechanism>PLAIN</mechanism>`)) {
+		t.Error("Expected mechanisms list to include PLAIN")
+	}
+	if !bytes.Contains((&b).Bytes(), []byte(`<mechanism>SCRAM-SHA-256</mechanism>`)) {
+		t.Error("Expected mechanisms list to include SCRAM-SHA-256")
+	}
+
+	// The wrapper can be a bit more flexible as long as the mechanisms are there.
+	d := xml.NewDecoder(&b)
+	tok, err := d.Token()
+	if err != nil {
+		t.Fatal(err)
+	}
+	se := tok.(xml.StartElement)
+	if se.Name.Local != "mechanisms" || se.Name.Space != NSSASL {
+		t.Errorf("Unexpected name for mechanisms start element: %+v", se.Name)
+	}
+	// Skip two mechanisms
+	tok, err = d.Token()
+	if err != nil {
+		t.Fatal(err)
+	}
+	d.Skip()
+	tok, err = d.Token()
+	if err != nil {
+		t.Fatal(err)
+	}
+	d.Skip()
+
+	// Check the end token.
+	tok, err = d.Token()
+	if err != nil {
+		t.Fatal(err)
+	}
+	_ = tok.(xml.EndElement)
 }
