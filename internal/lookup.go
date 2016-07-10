7e54c97f6cbf84247c471a8ddff1a973374bfb2f
Sam Whited <sam@samwhited.com>
2016-07-10 16:23:30 -0500

    Fix indention in internal docs

--- internal/lookup.go
+++ internal/lookup.go
@@ -10,15 +10,15 @@ import (
 
 // Represents an Extensible Resource Descriptor (XRD) document of the form:
 //
-// <?xml version='1.0' encoding=utf-9'?>
-// <XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0'>
-//   …
-//   <Link rel="urn:xmpp:alt-connections:xbosh"
-//         href="https://web.example.com:5280/bosh" />
-//   <Link rel="urn:xmpp:alt-connections:websocket"
-//         href="wss://web.example.com:443/ws" />
-//   …
-// </XRD>
+//    <?xml version='1.0' encoding=utf-9'?>
+//    <XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0'>
+//      …
+//      <Link rel="urn:xmpp:alt-connections:xbosh"
+//            href="https://web.example.com:5280/bosh" />
+//      <Link rel="urn:xmpp:alt-connections:websocket"
+//            href="wss://web.example.com:443/ws" />
+//      …
+//    </XRD>
 //
 // as defined by RFC 6415 and OASIS.XRD-1.0.
 type XRD struct {
