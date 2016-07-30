097abcffffc0aeccde14a51d30f430d891a023a2
Sam Whited <sam@samwhited.com>
2016-07-29 20:02:04 -0500

    Fix tests

--- config_test.go
+++ config_test.go
@@ -11,7 +11,7 @@ import (
 // The default value of config.conntype should return "xmpp-client"
 func TestDefaultConnType(t *testing.T) {
 	c := &Config{}
-	if ct := c.connType(); ct != "xmpp-client" {
+	if ct := connType(c.S2S); ct != "xmpp-client" {
 		t.Errorf("Wrong default value for conntype; expected xmpp-client but got %s", ct)
 	}
 }
@@ -19,7 +19,7 @@ func TestDefaultConnType(t *testing.T) {
 // If S2S is true, config.conntype should return "xmpp-server"
 func TestS2SConnType(t *testing.T) {
 	c := &Config{S2S: true}
-	if ct := c.connType(); ct != "xmpp-server" {
+	if ct := connType(c.S2S); ct != "xmpp-server" {
 		t.Errorf("Wrong s2s value for conntype; expected xmpp-server but got %s", ct)
 	}
 }
