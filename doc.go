35ec7395ae30d057c8c9225f22997d2dce878155
Sam Whited <sam@samwhited.com>
2017-07-02 12:16:14 -0500

    xmpp: update docs
    [ci skip]

--- doc.go
+++ doc.go
@@ -5,6 +5,17 @@
 // Package xmpp provides functionality from the Extensible Messaging and
 // Presence Protocol.
 //
+// It is subdivided into several packages; this package provides functionality
+// for establishing an XMPP session, feature negotiation (including an API for
+// defining your own stream features), and low-level connection and stream
+// manipulation. It allows sending and receiving "raw" XML over the stream but
+// there is no special support for stanzas, and RFC 6120 semantics are not
+// enforced. The jid package provides an implementation of the XMPP address
+// format defined in RFC 7622. Several other packages also exist which provide
+// functionality for common XMPP extensions. In future, a new package may
+// provide a higher level API around sending / receiving stanzas, enforcing RFC
+// 6120 and RFC 6121 semantics, etc.
+//
 // Be advised: This API is still unstable and is subject to change.
 package xmpp // import "mellium.im/xmpp"
 
