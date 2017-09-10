f047a0b9c35af72525e298c1b24dbf56921efaeb
Sam Whited <sam@samwhited.com>
2017-09-09 19:05:54 -0500

    stanza: simplify Message type

--- stanza/message_test.go
+++ stanza/message_test.go
@@ -1,87 +1,67 @@
 // Copyright 2015 Sam Whited.
-// Use of this source code is governed by the BSD 2-clause license that can be
-// found in the LICENSE file.
+// Use of this source code is governed by the BSD 2-clause
+// license that can be found in the LICENSE file.
 
-package stanza
+package stanza_test
 
 import (
+	"bytes"
 	"encoding/xml"
 	"fmt"
 	"testing"
 
-	"mellium.im/xmpp/jid"
+	"mellium.im/xmpp/stanza"
 )
 
-var (
-	_ fmt.Stringer        = (*messageType)(nil)
-	_ fmt.Stringer        = NormalMessage
-	_ xml.MarshalerAttr   = NormalMessage
-	_ xml.MarshalerAttr   = (*messageType)(nil)
-	_ xml.UnmarshalerAttr = (*messageType)(nil)
-)
+func TestMarshalMessageTypeAttr(t *testing.T) {
+	for i, tc := range [...]struct {
+		messagetype stanza.MessageType
+		value       string
+		err         error
+	}{
+		0: {stanza.MessageType(""), "", nil},
+		1: {stanza.NormalMessage, "normal", nil},
+		2: {stanza.ChatMessage, "chat", nil},
+		3: {stanza.HeadlineMessage, "headline", nil},
+		4: {stanza.ErrorMessage, "error", nil},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			b, err := xml.Marshal(stanza.Message{Type: tc.messagetype})
+			if err != tc.err {
+				t.Fatalf("Got unexpected error while marshaling Message: want='%v', got='%v'", tc.err, err)
+			}
 
-// TODO: Make this a table test and add some more complicated messages.
-// TODO: How should we test marshalling? Probably don't want to assume that
-//       attribute order will remain stable.
+			// Special case to check that empty values are omitted
+			if string(tc.messagetype) == "" {
+				if bytes.Contains(b, []byte("type")) {
+					t.Fatalf(`Didn't expect output to contain type attribute, found: %s`, b)
+				}
+				return
+			}
 
-func TestDefaults(t *testing.T) {
-	var mt messageType
-
-	if mt != NormalMessage {
-		t.Log("Default value of message type should be 'normal'.")
-		t.Fail()
+			if err == nil && !bytes.Contains(b, []byte(fmt.Sprintf(`type="%s"`, tc.messagetype))) {
+				t.Errorf(`Expected output to contain type="%s", found: %s`, tc.messagetype, b)
+			}
+		})
 	}
 }
 
-// Tests unmarshalling of a single XML blob into a message.
-func TestUnmarshalMessage(t *testing.T) {
-	mb := []byte(`
-<message
-    from='juliet@example.com/balcony'
-    id='ktx72v49'
-    to='romeo@example.net'
-    type='chat'
-    xml:lang='en'>
-  <body>Art thou not Romeo, and a Montague?</body>
-</message>
-	`)
-	m := &Message{
-		To:      &jid.JID{},
-		From:    &jid.JID{},
-		XMLName: xml.Name{},
-	}
-	err := xml.Unmarshal(mb, m)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if m.Type != ChatMessage {
-		t.Logf("Expected %s but got %s", ChatMessage, m.Type)
-		t.Fail()
-	}
-	if m.To.String() != "romeo@example.net" {
-		t.Errorf("Expected %s but got %s", "romeo@example.net", m.To.String())
-	}
-	if m.To.String() != "romeo@example.net" {
-		t.Errorf("Expected %s but got %s", "romeo@example.net", m.To.String())
-	}
-	if m.ID != "ktx72v49" {
-		t.Errorf("Expected %s but got %s", "ktx72v49", m.To.String())
-	}
-}
-
-// Messages must be marshalable to XML
-func TestMarshalMessage(t *testing.T) {
-	j := jid.MustParse("feste@shakespeare.lit")
-	m := Message{
-		ID:      "1234",
-		To:      j,
-		Lang:    "en",
-		XMLName: xml.Name{Space: "jabber:client", Local: "message"},
-	}
-	// TODO: Check the output; is the order guaranteed?
-	if _, err := xml.Marshal(m); err != nil {
-		t.Log(err)
-		t.Fail()
+func TestUnmarshalMessageTypeAttr(t *testing.T) {
+	for i, tc := range [...]struct {
+		message     string
+		messagetype stanza.MessageType
+	}{
+		0: {`<message type="normal"/>`, stanza.NormalMessage},
+		1: {`<message type="error"/>`, stanza.ErrorMessage},
+	} {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			message := stanza.Message{}
+			switch err := xml.Unmarshal([]byte(tc.message), &message); {
+			case err != nil:
+				t.Errorf("Got unexpected error while unmarshaling Message: %v", err)
+			case tc.messagetype != message.Type:
+				t.Errorf("Wrong type when unmarshaling Message: want=%s, got=%s", tc.messagetype, message.Type)
+			}
+		})
 	}
 }
