9239e365b3631f0cbb189adc97955c635204908a
Sam Whited <sam@samwhited.com>
2017-10-01 10:14:13 -0500

    xmpp: remove go:generate comment, tweak docs
    [ci skip]

--- doc.go
+++ doc.go
@@ -3,20 +3,14 @@
 // license that can be found in the LICENSE file.
 
 // Package xmpp provides functionality from the Extensible Messaging and
-// Presence Protocol.
+// Presence Protocol, formerly known as "Jabber".
 //
 // It is subdivided into several packages; this package provides functionality
 // for establishing an XMPP session, feature negotiation (including an API for
 // defining your own stream features), and low-level connection and stream
-// manipulation. It allows sending and receiving "raw" XML over the stream but
-// there is no special support for stanzas, and RFC 6120 semantics are not
-// enforced. The jid package provides an implementation of the XMPP address
-// format defined in RFC 7622. Several other packages also exist which provide
-// functionality for common XMPP extensions. In future, a new package may
-// provide a higher level API around sending / receiving stanzas, enforcing RFC
-// 6120 and RFC 6121 semantics, etc.
+// manipulation.
+// The jid package provides an implementation of the XMPP address format defined
+// in RFC 7622.
 //
 // Be advised: This API is still unstable and is subject to change.
 package xmpp // import "mellium.im/xmpp"
-
-//go:generate stringer -type=iqType,errorType,messageType,presenceType -output stanzatype_string.go
