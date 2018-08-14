abb43038710f47db89d55e7fac2bdf179291938a
Sam Whited <sam@samwhited.com>
2018-08-14 08:54:26 -0500

    xmpp: add missing copyright statement
    [ci skip]

--- echobot_example_test.go
+++ echobot_example_test.go
@@ -1,3 +1,7 @@
+// Copyright 2018 The Mellium Contributors.
+// Use of this source code is governed by the BSD 2-clause
+// license that can be found in the LICENSE file.
+
 package xmpp_test
 
 import (
