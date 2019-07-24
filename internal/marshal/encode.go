380dda6ea764be3d04a4135dc42018123cdc8d14
Sam Whited <sam@samwhited.com>
2019-07-24 10:49:08 -0500

    internal/marshal: add package description

--- internal/marshal/encode.go
+++ internal/marshal/encode.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by the BSD 2-clause
 // license that can be found in the LICENSE file.
 
+// Package marshal contains functions for encoding structs as an XML token
+// stream.
 package marshal
 
 import (
