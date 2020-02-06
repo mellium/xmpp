a21bad94d36f45dc81175c7f7f4fab9b4ac745a3
Sam Whited <sam@samwhited.com>
2020-02-06 11:26:04 -0500

    all: drop support for Go versions below 1.13
    Fixes #3

--- internal/discover/notfound.go
+++ internal/discover/notfound.go
@@ -2,16 +2,13 @@
 // Use of this source code is governed by the BSD 2-clause
 // license that can be found in the LICENSE file.
 
-// +build !go1.13
-
 package discover
 
 import (
 	"net"
-	"strings"
 )
 
 func isNotFound(err error) bool {
 	dnsErr, ok := err.(*net.DNSError)
-	return ok && strings.Contains(dnsErr.Error(), "no such host")
+	return ok && dnsErr.IsNotFound
 }
