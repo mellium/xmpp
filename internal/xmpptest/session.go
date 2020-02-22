df2fda88b9e74e19354271a35080540bf93e6bcc
Sam Whited <sam@samwhited.com>
2020-02-22 13:14:03 -0500

    internal/xmpptest: set default stream namespace
    Previously test sessions did not have a default namespace. Setting the
    namespace ensures that IQ and Stanza name checks won't break in the
    future when we start being more strict about validating namespaces.
    
    Signed-off-by: Sam Whited <sam@samwhited.com>

--- internal/xmpptest/session.go
+++ internal/xmpptest/session.go
@@ -8,23 +8,32 @@ package xmpptest // import "mellium.im/xmpp/internal/xmpptest"
 import (
 	"context"
 	"io"
+	"strings"
 
 	"mellium.im/xmpp"
+	"mellium.im/xmpp/internal/ns"
 	"mellium.im/xmpp/jid"
+	"mellium.im/xmpp/stream"
 )
 
 // NopNegotiator marks the state as ready (by returning state|xmpp.Ready) and
-// does not actually transmit any data over the wire or perform any other
-// session negotiation.
+// pops the first token (likely <stream:stream>) but does not perform any
+// validation on the token, transmit any data over the wire, or perform any
+// other session negotiation.
 func NopNegotiator(state xmpp.SessionState) xmpp.Negotiator {
-	return func(_ context.Context, _ *xmpp.Session, _ interface{}) (xmpp.SessionState, io.ReadWriter, interface{}, error) {
-		return state | xmpp.Ready, nil, nil, nil
+	return func(ctx context.Context, s *xmpp.Session, data interface{}) (xmpp.SessionState, io.ReadWriter, interface{}, error) {
+		// Pop the stream start token.
+		rc := s.TokenReader()
+		defer rc.Close()
+
+		_, err := rc.Token()
+		return state | xmpp.Ready, nil, nil, err
 	}
 }
 
-// NewSession returns a new XMPP session with the state bits set to
-// state|xmpp.Ready, the origin JID set to "test@example.net" and the location
-// JID set to "example.net".
+// NewSession returns a new client-to-client XMPP session with the state bits
+// set to state|xmpp.Ready, the origin JID set to "test@example.net" and the
+// location JID set to "example.net".
 //
 // NewSession panics on error for ease of use in testing, where a panic is
 // acceptable.
@@ -33,7 +42,19 @@ func NewSession(state xmpp.SessionState, rw io.ReadWriter) *xmpp.Session {
 	origin := jid.MustParse("test@example.net")
 
 	s, err := xmpp.NegotiateSession(
-		context.Background(), location, origin, rw, false,
+		context.Background(), location, origin,
+		struct {
+			io.Reader
+			io.Writer
+		}{
+			Reader: io.MultiReader(
+				strings.NewReader(`<stream:stream xmlns="`+ns.Client+`" xmlns:stream="`+stream.NS+`">`),
+				rw,
+				strings.NewReader(`</stream:stream>`),
+			),
+			Writer: rw,
+		},
+		false,
 		NopNegotiator(state),
 	)
 	if err != nil {
