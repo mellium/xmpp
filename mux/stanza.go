4db9569cab8fdb4cb57437ad0298cce4c97fb685
Sam Whited <sam@samwhited.com>
2020-11-21 20:20:25 -0500

    mux: remove unnecessary argument names
    Signed-off-by: Sam Whited <sam@samwhited.com>

--- mux/stanza.go
+++ mux/stanza.go
@@ -13,14 +13,14 @@ import (
 
 // IQHandler responds to IQ stanzas.
 type IQHandler interface {
-	HandleIQ(iq stanza.IQ, t xmlstream.TokenReadEncoder, start *xml.StartElement) error
+	HandleIQ(stanza.IQ, xmlstream.TokenReadEncoder, *xml.StartElement) error
 }
 
 // The IQHandlerFunc type is an adapter to allow the use of ordinary functions
 // as IQ handlers.
 // If f is a function with the appropriate signature, IQHandlerFunc(f) is an
 // IQHandler that calls f.
-type IQHandlerFunc func(iq stanza.IQ, t xmlstream.TokenReadEncoder, start *xml.StartElement) error
+type IQHandlerFunc func(stanza.IQ, xmlstream.TokenReadEncoder, *xml.StartElement) error
 
 // HandleIQ calls f(iq, t, start).
 func (f IQHandlerFunc) HandleIQ(iq stanza.IQ, t xmlstream.TokenReadEncoder, start *xml.StartElement) error {
@@ -29,14 +29,14 @@ func (f IQHandlerFunc) HandleIQ(iq stanza.IQ, t xmlstream.TokenReadEncoder, star
 
 // MessageHandler responds to message stanzas.
 type MessageHandler interface {
-	HandleMessage(msg stanza.Message, t xmlstream.TokenReadEncoder) error
+	HandleMessage(stanza.Message, xmlstream.TokenReadEncoder) error
 }
 
 // The MessageHandlerFunc type is an adapter to allow the use of ordinary
 // functions as message handlers.
 // If f is a function with the appropriate signature, MessageHandlerFunc(f) is a
 // MessageHandler that calls f.
-type MessageHandlerFunc func(msg stanza.Message, t xmlstream.TokenReadEncoder) error
+type MessageHandlerFunc func(stanza.Message, xmlstream.TokenReadEncoder) error
 
 // HandleMessage calls f(msg, t).
 func (f MessageHandlerFunc) HandleMessage(msg stanza.Message, t xmlstream.TokenReadEncoder) error {
@@ -45,14 +45,14 @@ func (f MessageHandlerFunc) HandleMessage(msg stanza.Message, t xmlstream.TokenR
 
 // PresenceHandler responds to message stanzas.
 type PresenceHandler interface {
-	HandlePresence(p stanza.Presence, t xmlstream.TokenReadEncoder) error
+	HandlePresence(stanza.Presence, xmlstream.TokenReadEncoder) error
 }
 
 // The PresenceHandlerFunc type is an adapter to allow the use of ordinary
 // functions as presence handlers.
 // If f is a function with the appropriate signature, PresenceHandlerFunc(f) is
 // a PresenceHandler that calls f.
-type PresenceHandlerFunc func(p stanza.Presence, t xmlstream.TokenReadEncoder) error
+type PresenceHandlerFunc func(stanza.Presence, xmlstream.TokenReadEncoder) error
 
 // HandlePresence calls f(p, t).
 func (f PresenceHandlerFunc) HandlePresence(p stanza.Presence, t xmlstream.TokenReadEncoder) error {
