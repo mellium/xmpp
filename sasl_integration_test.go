0b369450563609a498bc9c9d0844350a216b27eb
Sam Whited <sam@samwhited.com>
2021-01-15 18:33:26 -0500

    xmpp: add an integration test for a SASL server
    Signed-off-by: Sam Whited <sam@samwhited.com>

---
+++ sasl_integration_test.go
@@ -0,0 +1,48 @@
+// Copyright 2020 The Mellium Contributors.
+// Use of this source code is governed by the BSD 2-clause
+// license that can be found in the LICENSE file.
+
+//+build integration
+
+package xmpp_test
+
+import (
+	"context"
+	"testing"
+
+	"mellium.im/xmpp/internal/integration"
+	"mellium.im/xmpp/internal/integration/mcabber"
+	"mellium.im/xmpp/internal/integration/mellium"
+	"mellium.im/xmpp/jid"
+)
+
+func TestMain(m *testing.M) {
+	mellium.TestMain(m)
+}
+
+func TestIntegrationSASLClient(t *testing.T) {
+	const pass = "testpass"
+	melliumRun := mellium.Test(context.TODO(), t,
+		integration.Cert("localhost"),
+		mellium.ConfigFile(mellium.Config{
+			ListenC2S: true,
+		}),
+		integration.User(jid.MustParse("me@localhost"), pass),
+	)
+	melliumRun(integrationSASLClient)
+}
+
+func integrationSASLClient(ctx context.Context, t *testing.T, cmd *integration.Cmd) {
+	j, pass := cmd.User()
+	p := cmd.C2SPort()
+	mcabberRun := mcabber.Test(context.TODO(), t,
+		mcabber.ConfigFile(mcabber.Config{
+			JID:      j,
+			Password: pass,
+			Port:     p,
+		}),
+	)
+	mcabberRun(func(ctx context.Context, t *testing.T, cmd *integration.Cmd) {
+		t.Log("Connected successfully!")
+	})
+}
