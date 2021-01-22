b12903a4fa6792f5bde6f2871ca38991663768fa
Sam Whited <sam@samwhited.com>
2021-01-22 13:59:11 -0500

    websocket: test against Ejabberd
    Signed-off-by: Sam Whited <sam@samwhited.com>

--- websocket/integration_test.go
+++ websocket/integration_test.go
@@ -14,6 +14,7 @@ import (
 	"mellium.im/sasl"
 	"mellium.im/xmpp"
 	"mellium.im/xmpp/internal/integration"
+	"mellium.im/xmpp/internal/integration/ejabberd"
 	"mellium.im/xmpp/internal/integration/prosody"
 	"mellium.im/xmpp/ping"
 	"mellium.im/xmpp/websocket"
@@ -26,6 +27,35 @@ func TestIntegrationDialWebSocket(t *testing.T) {
 		prosody.ListenC2S(),
 	)
 	prosodyRun(integrationDialWebsocket)
+
+	ejabberdRun := ejabberd.Test(context.TODO(), t,
+		integration.Log(),
+		ejabberd.WebSocket(),
+		ejabberd.ListenC2S(),
+	)
+	ejabberdRun(integrationDialWebsocketUnix)
+}
+
+func integrationDialWebsocketUnix(ctx context.Context, t *testing.T, cmd *integration.Cmd) {
+	j, pass := cmd.User()
+	conn, err := cmd.HTTPConn(ctx, true)
+	if err != nil {
+		t.Fatalf("error getting HTTPS connection: %v", err)
+	}
+	session, err := websocket.NewClient(ctx, "wss://localhost/xmpp", "wss://localhost/xmpp", j, conn, xmpp.SASL("", pass, sasl.Plain), xmpp.BindResource())
+	if err != nil {
+		t.Fatalf("error negotiating session: %v", err)
+	}
+	go func() {
+		err := session.Serve(nil)
+		if err != nil {
+			t.Logf("error from serve: %v", err)
+		}
+	}()
+	err = ping.Send(ctx, session, session.RemoteAddr())
+	if err != nil {
+		t.Errorf("error pinging: %v", err)
+	}
 }
 
 func integrationDialWebsocket(ctx context.Context, t *testing.T, cmd *integration.Cmd) {
