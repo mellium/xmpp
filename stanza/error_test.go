abfb8d3799988d7fbc94450000b62337566a41d4
Sam Whited <sam@samwhited.com>
2021-02-18 20:26:53 -0500

    stanza: move tests into stanza_test package
    Signed-off-by: Sam Whited <sam@samwhited.com>

--- stanza/error_test.go
+++ stanza/error_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the BSD 2-clause
 // license that can be found in the LICENSE file.
 
-package stanza
+package stanza_test
 
 import (
 	"encoding/xml"
@@ -12,15 +12,16 @@ import (
 
 	"mellium.im/xmlstream"
 	"mellium.im/xmpp/jid"
+	"mellium.im/xmpp/stanza"
 )
 
 var (
-	_ error               = (*Error)(nil)
-	_ error               = Error{}
-	_ xmlstream.WriterTo  = (*Error)(nil)
-	_ xmlstream.WriterTo  = Error{}
-	_ xmlstream.Marshaler = (*Error)(nil)
-	_ xmlstream.Marshaler = Error{}
+	_ error               = (*stanza.Error)(nil)
+	_ error               = stanza.Error{}
+	_ xmlstream.WriterTo  = (*stanza.Error)(nil)
+	_ xmlstream.WriterTo  = stanza.Error{}
+	_ xmlstream.Marshaler = (*stanza.Error)(nil)
+	_ xmlstream.Marshaler = stanza.Error{}
 
 	simpleText = map[string]string{
 		"": "test",
@@ -28,11 +29,11 @@ var (
 )
 
 func TestErrorReturnsCondition(t *testing.T) {
-	s := Error{Condition: "leprosy"}
+	s := stanza.Error{Condition: "leprosy"}
 	if string(s.Condition) != s.Error() {
 		t.Errorf("Expected stanza error to return condition `leprosy` but got %s", s.Error())
 	}
-	s = Error{Condition: "nope", Text: map[string]string{
+	s = stanza.Error{Condition: "nope", Text: map[string]string{
 		"": "Text",
 	}}
 	if string(s.Condition) != s.Error() {
@@ -42,16 +43,16 @@ func TestErrorReturnsCondition(t *testing.T) {
 
 func TestMarshalStanzaError(t *testing.T) {
 	for i, data := range [...]struct {
-		se  Error
+		se  stanza.Error
 		xml string
 		err bool
 	}{
-		0: {Error{}, "", true},
-		1: {Error{Condition: UnexpectedRequest}, `<error><unexpected-request xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"></unexpected-request></error>`, false},
-		2: {Error{Type: Cancel, Condition: UnexpectedRequest}, `<error type="cancel"><unexpected-request xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"></unexpected-request></error>`, false},
-		3: {Error{Type: Wait, Condition: UndefinedCondition}, `<error type="wait"><undefined-condition xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"></undefined-condition></error>`, false},
-		4: {Error{Type: Modify, By: jid.MustParse("test@example.net"), Condition: SubscriptionRequired}, `<error type="modify" by="test@example.net"><subscription-required xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"></subscription-required></error>`, false},
-		5: {Error{Type: Continue, Condition: ServiceUnavailable, Text: simpleText}, `<error type="continue"><service-unavailable xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"></service-unavailable><text xmlns="urn:ietf:params:xml:ns:xmpp-stanzas">test</text></error>`, false},
+		0: {stanza.Error{}, "", true},
+		1: {stanza.Error{Condition: stanza.UnexpectedRequest}, `<error><unexpected-request xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"></unexpected-request></error>`, false},
+		2: {stanza.Error{Type: stanza.Cancel, Condition: stanza.UnexpectedRequest}, `<error type="cancel"><unexpected-request xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"></unexpected-request></error>`, false},
+		3: {stanza.Error{Type: stanza.Wait, Condition: stanza.UndefinedCondition}, `<error type="wait"><undefined-condition xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"></undefined-condition></error>`, false},
+		4: {stanza.Error{Type: stanza.Modify, By: jid.MustParse("test@example.net"), Condition: stanza.SubscriptionRequired}, `<error type="modify" by="test@example.net"><subscription-required xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"></subscription-required></error>`, false},
+		5: {stanza.Error{Type: stanza.Continue, Condition: stanza.ServiceUnavailable, Text: simpleText}, `<error type="continue"><service-unavailable xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"></service-unavailable><text xmlns="urn:ietf:params:xml:ns:xmpp-stanzas">test</text></error>`, false},
 	} {
 		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
 			b, err := xml.Marshal(data.se)
@@ -72,47 +73,47 @@ func TestMarshalStanzaError(t *testing.T) {
 func TestUnmarshalStanzaError(t *testing.T) {
 	for i, data := range [...]struct {
 		xml string
-		se  Error
+		se  stanza.Error
 		err bool
 	}{
-		0: {"", Error{}, true},
+		0: {"", stanza.Error{}, true},
 		1: {`<error><unexpected-request xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"></unexpected-request></error>`,
-			Error{Condition: UnexpectedRequest}, false},
+			stanza.Error{Condition: stanza.UnexpectedRequest}, false},
 		2: {`<error type="cancel"><registration-required xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"></registration-required></error>`,
-			Error{Type: Cancel, Condition: RegistrationRequired}, false},
+			stanza.Error{Type: stanza.Cancel, Condition: stanza.RegistrationRequired}, false},
 		3: {`<error type="cancel"><redirect xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"></redirect></error>`,
-			Error{Type: Cancel, Condition: Redirect}, false},
+			stanza.Error{Type: stanza.Cancel, Condition: stanza.Redirect}, false},
 		4: {`<error type="wait"><undefined-condition xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"></undefined-condition></error>`,
-			Error{Type: Wait, Condition: UndefinedCondition}, false},
+			stanza.Error{Type: stanza.Wait, Condition: stanza.UndefinedCondition}, false},
 		5: {`<error type="modify" by="test@example.net"><subscription-required xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"></subscription-required></error>`,
-			Error{Type: Modify, By: jid.MustParse("test@example.net"), Condition: SubscriptionRequired}, false},
+			stanza.Error{Type: stanza.Modify, By: jid.MustParse("test@example.net"), Condition: stanza.SubscriptionRequired}, false},
 		6: {`<error type="continue"><service-unavailable xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"></service-unavailable><text xmlns="urn:ietf:params:xml:ns:xmpp-stanzas">test</text></error>`,
-			Error{Type: Continue, Condition: ServiceUnavailable, Text: simpleText}, false},
+			stanza.Error{Type: stanza.Continue, Condition: stanza.ServiceUnavailable, Text: simpleText}, false},
 		7: {`<error type="auth"><resource-constraint xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"></resource-constraint><text xmlns="urn:ietf:params:xml:ns:xmpp-stanzas" xml:lang="en">test</text></error>`,
-			Error{Type: Auth, Condition: ResourceConstraint, Text: map[string]string{
+			stanza.Error{Type: stanza.Auth, Condition: stanza.ResourceConstraint, Text: map[string]string{
 				"en": "test",
 			}}, false},
 		8: {`<error type="auth"><resource-constraint xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"></resource-constraint><text xmlns="urn:ietf:params:xml:ns:xmpp-stanzas" xml:lang="en">test</text><text xmlns="urn:ietf:params:xml:ns:xmpp-stanzas" xml:lang="de">German</text></error>`,
-			Error{Type: Auth, Condition: ResourceConstraint, Text: map[string]string{
+			stanza.Error{Type: stanza.Auth, Condition: stanza.ResourceConstraint, Text: map[string]string{
 				"en": "test",
 				"de": "German",
 			}}, false},
 		9: {`<error type="auth"><remote-server-timeout xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"></remote-server-timeout><text xmlns="urn:ietf:params:xml:ns:xmpp-stanzas" xml:lang="en">test</text><text xmlns="urn:ietf:params:xml:ns:xmpp-stanzas" xml:lang="es">Spanish</text></error>`,
-			Error{Type: Auth, Condition: RemoteServerTimeout, Text: map[string]string{
+			stanza.Error{Type: stanza.Auth, Condition: stanza.RemoteServerTimeout, Text: map[string]string{
 				"en": "test",
 				"es": "Spanish",
 			}}, false},
 		10: {`<error by=""><remote-server-not-found xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"></remote-server-not-found></error>`,
-			Error{By: jid.JID{}, Condition: RemoteServerNotFound}, false},
+			stanza.Error{By: jid.JID{}, Condition: stanza.RemoteServerNotFound}, false},
 		11: {`<error><other xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"></other></error>`,
-			Error{Condition: Condition("other")}, false},
+			stanza.Error{Condition: stanza.Condition("other")}, false},
 		12: {`<error><recipient-unavailable xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"></recipient-unavailable><text xmlns="urn:ietf:params:xml:ns:xmpp-stanzas" xml:lang="ac-u">test</text></error>`,
-			Error{Condition: RecipientUnavailable, Text: map[string]string{
+			stanza.Error{Condition: stanza.RecipientUnavailable, Text: map[string]string{
 				"ac-u": "test",
 			}}, false},
 	} {
 		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
-			se2 := Error{}
+			se2 := stanza.Error{}
 			err := xml.Unmarshal([]byte(data.xml), &se2)
 			j1, j2 := data.se.By, se2.By
 			data.se.By = jid.JID{}
