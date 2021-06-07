86206466fdac52e60bb711d24b726c8bbc493220
Sam Whited <sam@samwhited.com>
2021-06-07 07:45:38 -0400

    disco/info: new package containing disco features
    Previously we had a disco.Feature type. However, packages may want to
    provide their feature as a convenience to be easily registered in the
    future. If they do this, we will create import loops between any
    packages that are already imported by disco (ie. jid and paging). Moving
    features out to its own info package will allow other packages to import
    it without bringing in all of the disco package.
    
    Signed-off-by: Sam Whited <sam@samwhited.com>

---
+++ disco/info/feature.go
@@ -0,0 +1,52 @@
+// Copyright 2021 The Mellium Contributors.
+// Use of this source code is governed by the BSD 2-clause
+// license that can be found in the LICENSE file.
+
+// Package info contains service discovery features.
+//
+// These were separated out into a separate package to prevent import loops.
+package info // import "mellium.im/xmpp/disco/info"
+
+import (
+	"encoding/xml"
+
+	"mellium.im/xmlstream"
+)
+
+const (
+	ns = `http://jabber.org/protocol/disco#info`
+)
+
+// Feature represents a feature supported by an entity on the network.
+type Feature struct {
+	XMLName xml.Name `xml:"http://jabber.org/protocol/disco#info feature"`
+	Var     string   `xml:"var,attr"`
+}
+
+// TokenReader implements xmlstream.Marshaler.
+func (f Feature) TokenReader() xml.TokenReader {
+	return xmlstream.Wrap(nil, xml.StartElement{
+		Name: xml.Name{Space: ns, Local: "feature"},
+		Attr: []xml.Attr{{
+			Name:  xml.Name{Local: "var"},
+			Value: f.Var,
+		}},
+	})
+}
+
+// WriteXML implements xmlstream.WriterTo.
+func (f Feature) WriteXML(w xmlstream.TokenWriter) (int, error) {
+	return xmlstream.Copy(w, f.TokenReader())
+}
+
+// MarshalXML implements xml.Marshaler.
+func (f Feature) MarshalXML(e *xml.Encoder, _ xml.StartElement) error {
+	_, err := f.WriteXML(e)
+	return err
+}
+
+// FeatureIter is the interface implemented by types that implement disco
+// features.
+type FeatureIter interface {
+	ForFeatures(node string, f func(Feature) error) error
+}
