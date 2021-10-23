a65455fd9997fc77556cb80fc6f8c940cd13f33a
Sam Whited <sam@samwhited.com>
2021-10-23 13:44:15 -0400

    internal/pubsub: add PublishIQ
    Signed-off-by: Sam Whited <sam@samwhited.com>

--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -25,6 +25,13 @@ type publishResponse struct {
 // Publish copies the first element from the provided token reader to a node on
 // the server from which it can be retrieved later.
 func Publish(ctx context.Context, s *xmpp.Session, node, id string, item xml.TokenReader) (string, error) {
+	return PublishIQ(ctx, s, stanza.IQ{}, node, id, item)
+}
+
+// PublishIQ is like Publish except that it allows modifying the IQ.
+// Changes to the IQ type will have no effect.
+func PublishIQ(ctx context.Context, s *xmpp.Session, iq stanza.IQ, node, id string, item xml.TokenReader) (string, error) {
+	iq.Type = stanza.SetIQ
 	start, err := item.Token()
 	if err != nil {
 		return "", err
