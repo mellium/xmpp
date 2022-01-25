9e34252fd8243f22d906eae9a78f299c8f14559b
Sam Whited <sam@samwhited.com>
2022-01-25 10:30:02 -0500

    examples/msgrepl: update linked issues
    Several issues were linked in comments straight to the GitHub issue
    tracker. This updates them to link to our own redirects instead (in case
    we ever move the issue tracker in the future we won't have broken links
    because we'll update our redirects).
    
    Signed-off-by: Sam Whited <sam@samwhited.com>

--- examples/msgrepl/main.go
+++ examples/msgrepl/main.go
@@ -98,8 +98,8 @@ func main() {
 	go func() {
 		err := session.Serve(xmpp.HandlerFunc(func(t xmlstream.TokenReadEncoder, start *xml.StartElement) error {
 
-			// This is a workaround for https://github.com/mellium/xmpp/issues/196
-			// until a cleaner permanent fix is devised (see https://github.com/mellium/xmpp/issues/197)
+			// This is a workaround for https://mellium.im/issue/196
+			// until a cleaner permanent fix is devised (see https://mellium.im/issue/197)
 			d := xml.NewTokenDecoder(xmlstream.MultiReader(xmlstream.Token(*start), t))
 			if _, err := d.Token(); err != nil {
 				return err
