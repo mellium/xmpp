f0efbd2fcd290e0b864f1a82544aeb3296dec265
Sam Whited <sam@samwhited.com>
2022-02-03 21:11:42 -0500

    internal/integration/slixmpp: new testing package
    Adds a new package that can be used to write integration tests against
    Slixmpp.
    
    Fixes #251
    
    Signed-off-by: Sam Whited <sam@samwhited.com>

---
+++ internal/integration/slixmpp/slixmpp.go
@@ -0,0 +1,86 @@
+// Copyright 2022 The Mellium Contributors.
+// Use of this source code is governed by the BSD 2-clause
+// license that can be found in the LICENSE file.
+
+// Package slixmpp facilitates integration testing against slixmpp.
+//
+// Tests are accomplished by importing the automatically generated
+// slixmpp_client Python package, subclassing the Daemon class, and overriding
+// the run method.
+// Other methods can also be overridden, in particular the prepare_argparse
+// method can be used to add command line arguments to the Python scripts.
+// For example:
+//
+//     from slixmpp_client import Daemon
+//     import slixmpp
+//
+//
+//     class Ping(Daemon):
+//         def prepare_argparse(self) -> None:
+//             super().prepare_argparse()
+//
+//             self.argparse.add_argument(
+//                 "-j",
+//                 type=slixmpp.jid.JID,
+//                 help="The JID to ping",
+//             )
+//
+//         def configure(self):
+//             super().configure()
+//             self.client.register_plugin('xep_0199') # Ping
+//
+//         async def run(self) -> None:
+//             await self.client.plugin['xep_0199'].ping(jid=self.args.j)
+//
+// For more information see slixmpp_client.py, python/xmpptest.py, and the
+// slixmpp documentation.
+package slixmpp // import "mellium.im/xmpp/internal/integration/slixmpp"
+
+import (
+	"context"
+	_ "embed"
+	"io"
+	"testing"
+	"text/template"
+
+	"mellium.im/xmpp/internal/integration"
+	"mellium.im/xmpp/internal/integration/python"
+)
+
+const (
+	baseFileName = "slixmpp_client.py"
+)
+
+var (
+	//go:embed slixmpp_client.py
+	baseTest string
+)
+
+func getConfig(cmd *integration.Cmd) python.Config {
+	if cmd.Config == nil {
+		cmd.Config = python.Config{}
+	}
+	return cmd.Config.(python.Config)
+}
+
+func defaultConfig(cmd *integration.Cmd) error {
+	tmpl, err := template.New("slixmpp").Parse(baseTest)
+	if err != nil {
+		return err
+	}
+
+	return integration.TempFile(baseFileName, func(cmd *integration.Cmd, w io.Writer) error {
+		cfg := getConfig(cmd)
+		return tmpl.Execute(w, cfg)
+	})(cmd)
+}
+
+// Test starts the slixmpp wrapper script and returns a function that runs
+// subtests using t.Run.
+// Multiple calls to the returned function will result in uniquely named
+// subtests.
+// When all subtests have completed, the daemon is stopped.
+func Test(ctx context.Context, t *testing.T, opts ...integration.Option) integration.SubtestRunner {
+	opts = append(opts, defaultConfig)
+	return python.Test(ctx, t, opts...)
+}
---
+++ internal/integration/slixmpp/slixmpp_client.py
@@ -0,0 +1,43 @@
+# Copyright 2022 The Mellium Contributors.
+# Use of this source code is governed by the BSD 2-clause
+# license that can be found in the LICENSE file.
+
+from slixmpp import jid
+import slixmpp
+import xmpptest
+
+
+###
+# This script acts as a wrapper around slixmpp that starts a simple client.
+# It is not meant to do any testing itself, instead use the Go python.Import
+# option to create a test script that subclasses Daemon and overrides its run
+# method.
+###
+
+class Daemon(xmpptest.Daemon):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def configure(self):
+        super().configure()
+        self.jid: jid.JID = jid.JID(self.config.get("client", "jid"))
+        self.xmpp_port = self.config.getint("client", "port")
+        self.client: slixmpp.ClientXMPP = slixmpp.ClientXMPP(
+            self.jid,
+            self.config.get("client", "password"),
+        )
+
+    async def run_test(self) -> None:
+        async def run_callback(_):
+            await self.run()
+        self.client.add_event_handler('session_start', run_callback)
+        self.client.connect(address=("127.0.0.1", self.xmpp_port))
+        await self.client.wait_until('session_end')
+
+    async def run(self) -> None:
+        """
+        The run method should be overridden by the test script and perform any
+        actions required for testing such as listening for incoming messages and
+        responding using self.client.
+        """
+        raise NotImplementedError("test file must override run function")
