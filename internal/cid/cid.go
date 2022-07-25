f7515f420eb83022ac77a195fbffd880047a441a
Sam Whited <sam@samwhited.com>
2022-07-25 10:05:49 -0400

    internal/cid: new package for parsing CID URLs
    Signed-off-by: Sam Whited <sam@samwhited.com>

---
+++ internal/cid/cid.go
@@ -0,0 +1,80 @@
+// Copyright 2022 The Mellium Contributors.
+// Use of this source code is governed by the BSD 2-clause
+// license that can be found in the LICENSE file.
+
+// Package cid implements content-ID URLs.
+package cid // import "mellium.im/xmpp/internal/cid"
+
+import (
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"net/url"
+)
+
+// Parse parses a raw url into a URL structure.
+//
+// The URL may or may not include the cid: scheme, but will fail if the exact
+// structure for CID URLs is not met (or if the scheme is present and is
+// anything other than "cid").
+func Parse(rawURL string) (*URL, error) {
+	u, err := url.Parse(rawURL)
+	if err != nil {
+		return nil, err
+	}
+	var opaque string
+	switch u.Scheme {
+	case "":
+		opaque = u.Path
+	case "cid":
+		opaque = u.Opaque
+	default:
+		return nil, fmt.Errorf("cid: failed to parse URL with scheme %q", u.Scheme)
+	}
+	if opaque == "" {
+		return nil, errors.New("cid: URL is invalid and resulted in empty CID")
+	}
+	var plusIdx, atIdx int
+	for i, b := range opaque {
+		switch b {
+		case '+':
+			plusIdx = i
+		case '@':
+			if plusIdx <= 0 {
+				return nil, errors.New("cid: missing hash name")
+			}
+			atIdx = i
+			break
+		}
+	}
+	if atIdx <= 0 || atIdx == len(opaque)-1 {
+		return nil, errors.New("cid: missing domain part")
+	}
+
+	out := &URL{
+		HashName: opaque[:plusIdx],
+		Domain:   opaque[atIdx+1:],
+	}
+	hash, err := hex.DecodeString(opaque[plusIdx+1 : atIdx])
+	if err != nil {
+		return nil, fmt.Errorf("cid: error decoding hash: %w", err)
+	}
+	out.Hash = hash
+	if len(hash) == 0 {
+		return nil, errors.New("cid: no hash found")
+	}
+
+	return out, nil
+}
+
+// A URL represents a parsed CID URL.
+type URL struct {
+	HashName string
+	Domain   string
+	Hash     []byte
+}
+
+// String reassembles the URL into a valid URL string.
+func (u *URL) String() string {
+	return fmt.Sprintf("cid:%s+%x@%s", u.HashName, u.Hash, u.Domain)
+}
---
+++ internal/cid/cid_test.go
@@ -0,0 +1,114 @@
+// Copyright 2022 The Mellium Contributors.
+// Use of this source code is governed by the BSD 2-clause
+// license that can be found in the LICENSE file.
+
+package cid_test
+
+import (
+	"encoding/hex"
+	"net/url"
+	"strconv"
+	"testing"
+
+	"mellium.im/xmpp/internal/cid"
+)
+
+var parseTestCases = []struct {
+	in       string
+	out      string
+	hashName string
+	domain   string
+	hash     string
+	errText  string
+}{
+	0: {
+		in:       "cid:name+abcd@domain",
+		hashName: "name",
+		domain:   "domain",
+		hash:     "abcd",
+	},
+	1: {
+		in:       "n+ab@d",
+		out:      "cid:n+ab@d",
+		hashName: "n",
+		domain:   "d",
+		hash:     "ab",
+	},
+	2: {
+		in:      "cid:+abcd@domain",
+		errText: "cid: missing hash name",
+	},
+	3: {
+		in:      "cid:abcd@domain",
+		errText: "cid: missing hash name",
+	},
+	4: {
+		in:      "cid:name+@domain",
+		errText: "cid: no hash found",
+	},
+	5: {
+		in:      "cid:name+ab@",
+		errText: "cid: missing domain part",
+	},
+	6: {
+		in:      "cid:name+ab",
+		errText: "cid: missing domain part",
+	},
+	7: {
+		in:      "cid:wat",
+		errText: "cid: missing domain part",
+	},
+	8: {
+		in:      "cid:sha1+wat@bob.xmpp.org",
+		errText: "cid: error decoding hash: encoding/hex: invalid byte: U+0077 'w'",
+	},
+	9: {
+		in:      "https://example.com",
+		errText: `cid: failed to parse URL with scheme "https"`,
+	},
+	10: {
+		in:      "cid://test:123",
+		errText: `cid: URL is invalid and resulted in empty CID`,
+	},
+	11: {
+		in: "%gh&%ij",
+		errText: func() string {
+			_, err := url.Parse("%gh&%ij")
+			return err.Error()
+		}(),
+	},
+}
+
+func TestParse(t *testing.T) {
+	for i, tc := range parseTestCases {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			u, err := cid.Parse(tc.in)
+			if tc.errText != "" && err == nil {
+				t.Fatalf("expected error during parsing")
+			}
+			if tc.errText != "" {
+				if err.Error() != tc.errText {
+					t.Fatalf("unexpected error text parsing CID: want=%v, got=%v", tc.errText, err.Error())
+				}
+				return
+			}
+
+			if u.HashName != tc.hashName {
+				t.Errorf("wrong hash name: want=%v, got=%v", tc.hashName, u.HashName)
+			}
+			if u.Domain != tc.domain {
+				t.Errorf("wrong domain: want=%v, got=%v", tc.domain, u.Domain)
+			}
+			if hash := hex.EncodeToString(u.Hash); hash != tc.hash {
+				t.Errorf("wrong hash: want=%v, got=%v", tc.hash, hash)
+			}
+			out := tc.out
+			if out == "" {
+				out = tc.in
+			}
+			if s := u.String(); s != out {
+				t.Errorf("wrong string output: want=%v, got=%v", out, s)
+			}
+		})
+	}
+}
