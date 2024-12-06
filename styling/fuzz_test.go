ce5f8b48252551899a4c2d9ef5b9c302e0650479
Sam Whited <sam@samwhited.com>
2024-12-06 11:32:11 -0500

    styling: mov fuzz tests out from behind build tag
    Also fix them to work with the current version of the API.
    
    Signed-off-by: Sam Whited <sam@samwhited.com>

--- styling/fuzz_test.go
+++ styling/fuzz_test.go
@@ -2,13 +2,11 @@
 // Use of this source code is governed by the BSD 2-clause
 // license that can be found in the LICENSE file.
 
-//go:build gofuzzbeta
-// +build gofuzzbeta
-
 package styling_test
 
 import (
 	"bytes"
+	"errors"
 	"io"
 	"testing"
 
@@ -26,22 +24,21 @@ func FuzzParseDocument(f *testing.F) {
 	f.Fuzz(func(t *testing.T, doc []byte) {
 		d := styling.NewDecoder(bytes.NewReader(doc))
 		n := 0
-		for {
-			tok, err := d.Token()
-			if err == io.EOF {
-				if n+len(tok.Data) != len(doc) {
-					t.Fatalf("got early EOF at %d for input:\n%v", n, doc)
-				}
-				break
-			}
-			if err != nil {
-				t.Fatalf("error decoding: %v\nOriginal bytes:\n%v", err, doc)
-				break
-			}
+		for d.Next() {
+			tok := d.Token()
 			if !bytes.Equal([]byte(doc[n:n+len(tok.Data)]), tok.Data) {
 				t.Fatalf("output bytes did not equal input bytes at %d for input:\n%v", n, doc)
 			}
 			n += len(tok.Data)
+		}
+		switch err := d.Err(); {
+		case errors.Is(err, io.EOF):
+			if n != len(doc) {
+				t.Fatalf("got early EOF at %d", n)
+			}
+		case err == nil:
+		default:
+			t.Fatalf("error decoding: %v\nOriginal bytes:\n%v", err, doc)
 		}
 	})
 }
